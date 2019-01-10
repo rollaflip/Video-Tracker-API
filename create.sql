b75cb7dc1867bd81d811caa7b6c6b3c984a5ca6f
Yen <iknepper@hotmail.com>
2019-01-10 02:00:15 -0500

    post and get routes set with dummy data & Joi installed for validation

--- create.sql
+++ create.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS `Videos` (
   `name` carchar(255) NOT NULL,
   `brand` carchar(255)NOT NULL,
   `published` date NOT NULL,
-  'viewcount' int(10) DEFAULT 0,
+  'viewCount' int(10) DEFAULT 0,
   PRIMARY KEY(`id`)
 );
--- server/api/app.js
+++ server/api/app.js
@@ -1,35 +1,68 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const Joi = require('joi')
+
+app.use(express.json());
 
 const videos = [
-  {id: 1, name: 'vid1', brand: 'Thrillist', published: Date.now(), count:5},
-  {id: 2, name: 'vid2', brand: 'Thrillist', published: Date.now(), count:5},
-  {id: 3, name: 'vid3', brand: 'Thrillist', published: Date.now(), count:5},
-
-]
+  {
+    id: 1,
+    name: 'vid1',
+    brand: 'Thrillist',
+    published: Date.now(),
+    viewCount: 5,
+  },
+  {
+    id: 2,
+    name: 'vid2',
+    brand: 'Thrillist',
+    published: Date.now(),
+    viewCount: 5,
+  },
+  {
+    id: 3,
+    name: 'vid3',
+    brand: 'Thrillist',
+    published: Date.now(),
+    viewCount: 5,
+  },
+];
 app.get('/', (req, res, next) => {
-  res.send('get req hit')
-})
-
+  res.send('get req hit');
+});
 
 app.get('/api/videos', (req, res, next) => {
-  res.send('[1,2,3,4]')
-})
+  res.send(videos);
+});
 
 //app/videos/`id`
 app.get('/api/videos/:id', (req, res, next) => {
-  res.send(req.params.id)
-})
-
-
-const port  = process.env.PORT || 3000
-app.listen(port, ()=> console.log(`listening on port ${port}...`))
+  const video = videos.find(vid => vid.id === parseInt(req.params.id));
+  if (!video) res.status(404).send('The video with the given ID was not found');
+  else res.send(video);
+});
 
 //create vid
-app.post('/', (req, res, next) => {
-})
+app.post('/api/videos', (req, res, next) => {
+  if(!req.body.name || req.body.name.length<1){
+    //400 bad request
+    res.status(400).send('Name is required')
+    return
+  }
+  const video = {
+    id: videos.length + 1, //dummy db id for now
+    name: req.body.name,
+  };
+  videos.push(video); //to dummy db
+  res.send(video)
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
 
 //add a view to video by id
 app.put('/', (req, res, next) => {
-})
+  const video = {
 
+  }
+});
